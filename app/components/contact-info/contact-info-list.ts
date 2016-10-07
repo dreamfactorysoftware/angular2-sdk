45526e16f19f3bce16b5c0daee14eaa162be2e9b
Javascript Boss <javascriptboss99@gmail.com>
2016-10-08 01:13:49 +0530

    Adding session-token/content-type for API calls

--- app/components/contact-info/contact-info-list.ts
+++ app/components/contact-info/contact-info-list.ts
@@ -1,63 +1,72 @@
-import {Component} from 'angular2/core';
-import {NgClass} from 'angular2/common';
-import {ROUTER_DIRECTIVES, Router, RouteParams} from 'angular2/router';
-import {URLSearchParams} from 'angular2/http';
+import { Component } from 'angular2/core';
+import { NgClass } from 'angular2/common';
+import { ROUTER_DIRECTIVES, Router, RouteParams } from 'angular2/router';
+import { URLSearchParams } from 'angular2/http';
 
-import {ContactInfo} from '../../models/contact-info';
-import {ContactInfoService} from '../../services/contact-info';
-import {BaseHttpService} from '../../services/base-http';
+import { ContactInfo } from '../../models/contact-info';
+import { ContactInfoService } from '../../services/contact-info';
+import { BaseHttpService } from '../../services/base-http';
 
 
 
 
 @Component({
-  selector: 'contact-info-list',
-  templateUrl: './components/contact-info/contact-info-list.html',
-  styleUrls: ['./components/contact-info/contact-info.css'],
-  providers: [ContactInfoService, BaseHttpService],
-  directives: [ROUTER_DIRECTIVES, NgClass]
+    selector: 'contact-info-list',
+    templateUrl: './components/contact-info/contact-info-list.html',
+    styleUrls: ['./components/contact-info/contact-info.css'],
+    providers: [ContactInfoService, BaseHttpService],
+    directives: [ROUTER_DIRECTIVES, NgClass]
 })
 
 export class ContactInfoListCmp {
-	public contactInfo: ContactInfo[] = [];
-	public contactInfoFields: any[] = [
-		{ label: 'ID', key: 'id' },
-		{ label: 'Type', key: 'infoType', mobileShow: true },
-		{ label: 'Phone', key: 'phone' },
-		{ label: 'Email', key: 'email' },
-		{ label: 'Address', key: 'address' },
-		{ label: 'City', key: 'city' },
-		{ label: 'State', key: 'state' },
-		{ label: 'Country', key: 'country' },
-	];
-	params: URLSearchParams = new URLSearchParams();
+    public contactInfo: ContactInfo[] = [];
+    public contactInfoFields: any[] = [
+        { label: 'ID', key: 'id' },
+        { label: 'Type', key: 'infoType', mobileShow: true },
+        { label: 'Phone', key: 'phone' },
+        { label: 'Email', key: 'email' },
+        { label: 'Address', key: 'address' },
+        { label: 'City', key: 'city' },
+        { label: 'State', key: 'state' },
+        { label: 'Country', key: 'country' },
+    ];
+    params: URLSearchParams = new URLSearchParams();
 
-	constructor (private contactInfoService: ContactInfoService, private routeParams: RouteParams, private router: Router) {
-		this.params.set('filter', 'contact_id=' + routeParams.get('id'));
-		this.getList();	
-	};
+    constructor(private contactInfoService: ContactInfoService, private routeParams: RouteParams, private router: Router) {
+        var token = localStorage.getItem('session_token');
+        if (token === '' || token === null) {
+            this.logout();
+        } else {
+            this.params.set('filter', 'contact_id=' + routeParams.get('id'));
+            this.getList();
+        }
 
-	getList () {
-		let self = this;
-		this.contactInfoService.query(this.params)
-			.subscribe((contactInfo: ContactInfo[]) => {
-				self.contactInfo = contactInfo;
-			});
-	};
+    };
+    logout() {
+        localStorage.setItem('session_token', '');
+        this.router.navigate(['Login']);
+    };
+    getList() {
+        let self = this;
+        this.contactInfoService.query(this.params)
+            .subscribe((contactInfo: ContactInfo[]) => {
+                self.contactInfo = contactInfo;
+            });
+    };
 
-	edit (id) {
-		this.router.navigate(['/ContactInfo', { id: id, contactId: this.routeParams.get('id') }]);
-	};
+    edit(id) {
+        this.router.navigate(['/ContactInfo', { id: id, contactId: this.routeParams.get('id') }]);
+    };
 
-	add () {
-		this.router.navigate(['/NewContactInfo', { contactId: this.routeParams.get('id') }]);	
-	}
+    add() {
+        this.router.navigate(['/NewContactInfo', { contactId: this.routeParams.get('id') }]);
+    }
 
-	remove (id) {
-		let self = this;
-		this.contactInfoService.remove(id)
-			.subscribe((id: any) => {
-				self.contactInfo = self.contactInfo.filter((item) => item.id !== id);
-			});
-	};
+    remove(id) {
+        let self = this;
+        this.contactInfoService.remove(id)
+            .subscribe((id: any) => {
+                self.contactInfo = self.contactInfo.filter((item) => item.id !== id);
+            });
+    };
 }
--- app/components/contact-info/contact-info.ts
+++ app/components/contact-info/contact-info.ts
@@ -1,84 +1,99 @@
-import {Component} from 'angular2/core';
-import {RouteParams, Router} from 'angular2/router';
-import {FORM_DIRECTIVES, FormBuilder, Validators, Control, ControlGroup} from 'angular2/common';
-
-import {ContactInfo} from '../../models/contact-info';
-import {ContactInfoService} from '../../services/contact-info';
-import {BaseHttpService} from '../../services/base-http';
+import { Component } from 'angular2/core';
+import { RouteParams, Router } from 'angular2/router';
+import { FORM_DIRECTIVES, FormBuilder, Validators, Control, ControlGroup } from 'angular2/common';
+import { ContactInfo } from '../../models/contact-info';
+import { ContactInfoService } from '../../services/contact-info';
+import { BaseHttpService } from '../../services/base-http';
 import * as constants from '../../config/constants';
-import {NotificationService} from '../../services/notification';
-import {BrowserDomAdapter} from 'angular2/platform/browser';
-
-
+import { NotificationService } from '../../services/notification';
+import { BrowserDomAdapter } from 'angular2/platform/browser';
+import { Headers, RequestOptions } from 'angular2/http';
+import { ValidationService } from '../../services/validation';
 
 @Component({
-	selector: 'contact-info',
-	templateUrl: './components/contact-info/contact-info.html',
-	styleUrls: ['./components/contact-info/contact-info.css'],
-	providers: [ContactInfoService, BaseHttpService, NotificationService, BrowserDomAdapter],
-	directives: [FORM_DIRECTIVES]
+    selector: 'contact-info',
+    templateUrl: './components/contact-info/contact-info.html',
+    styleUrls: ['./components/contact-info/contact-info.css'],
+    providers: [ContactInfoService, BaseHttpService, NotificationService, BrowserDomAdapter],
+    directives: [FORM_DIRECTIVES]
 })
 
 export class ContactInfoCmp {
-	form: ControlGroup;
+    form: ControlGroup;
 
-	id = new Control('');
-	address = new Control('', Validators.required);
-	infoType = new Control('', Validators.required);
-	city = new Control('', Validators.required);
-	state = new Control('', Validators.required);
-	country = new Control('', Validators.required);
-	email = new Control('', Validators.required);
-	phone = new Control('', Validators.required);
-	zip = new Control('', Validators.required);
+    id = new Control('');
+    address = new Control('', Validators.compose([Validators.minLength(2), Validators.maxLength(50), Validators.required]));
+    infoType = new Control('', Validators.required);
+    city = new Control('', Validators.compose([Validators.minLength(2), Validators.maxLength(50), Validators.required]));
+    state = new Control('', Validators.compose([Validators.minLength(2), Validators.maxLength(20), Validators.required]));
+    country = new Control('', Validators.compose([Validators.minLength(2), Validators.maxLength(20), Validators.required]));
+    email = new Control('', Validators.compose([Validators.maxLength(50), ValidationService.emailValidator, Validators.required]));
+    phone = new Control('', Validators.compose([Validators.maxLength(50), Validators.minLength(10), ValidationService.phoneNumberValidator,
+        Validators.required
+    ]));
+    zip = new Control('', Validators.compose([Validators.maxLength(50), Validators.minLength(5), ValidationService.zipCodeValidator,
+        Validators.required
+    ]));
 
-	infoTypes = ['home', 'work', 'mobile'];
+    infoTypes = ['home', 'work', 'mobile'];
 
-	contactInfo: ContactInfo = new ContactInfo();
+    contactInfo: ContactInfo = new ContactInfo();
 
 
-	constructor(private contactInfoService: ContactInfoService, private router:Router, private params: RouteParams, private formBuilder: FormBuilder, private httpService: BaseHttpService, private notificationService: NotificationService) {
+    constructor(private contactInfoService: ContactInfoService, private router: Router, private params: RouteParams, private formBuilder: FormBuilder,
+        private httpService: BaseHttpService, private notificationService: NotificationService) {
+        var token = localStorage.getItem('session_token');
+        if (token === '' || token === null) {
+            this.logout();
+        }
+        var id: string = params.get('id');
+        this.contactInfo.contactId = params.get('contactId');
+        if (id) {
+            let self = this;
+            contactInfoService
+                .get(id)
+                .subscribe((contactInfo) => self.contactInfo = contactInfo);
+        }
 
-		var id: string = params.get('id');
-		this.contactInfo.contactId = params.get('contactId');
-		if (id) {
-			let self = this;
-			contactInfoService
-				.get(id)
-				.subscribe((contactInfo) => self.contactInfo = contactInfo);
-		}
+        this.form = this.formBuilder.group({
+            address: this.address,
+            infoType: this.infoType,
+            city: this.city,
+            state: this.state,
+            country: this.country,
+            email: this.email,
+            phone: this.phone,
+            zip: this.zip
+        });
+    };
+    logout() {
+        localStorage.setItem('session_token', '');
+        this.router.navigate(['Login']);
+    };
+    save() {
+        var self = this;
+        var queryHeaders = new Headers();
+        queryHeaders.append('Content-Type', 'application/json');
+        queryHeaders.append('X-Dreamfactory-Session-Token', localStorage.getItem('session_token'));
+        queryHeaders.append('X-Dreamfactory-API-Key', constants.DREAMFACTORY_API_KEY);
+        let options = new RequestOptions({ headers: queryHeaders });
+        if (this.contactInfo.id) {
+            this.httpService.http.patch(constants.DREAMFACTORY_INSTANCE_URL + '/api/v2/db/_table/contact_info', this.contactInfo.toJson(true), options)
+                .subscribe((data) => {
+                    this.notificationService.show('success', 'Contact Info Updated!');
+                });
+        } else {
+            delete this.contactInfo.id;
+            this.httpService.http.post(constants.DREAMFACTORY_INSTANCE_URL + '/api/v2/db/_table/contact_info/', this.contactInfo.toJson(true), options)
+                .subscribe((data) => {
+                    this.notificationService.show('success', 'New Contact Info Created!');
+                    self.back();
+                });
+        }
 
-		this.form = this.formBuilder.group({
-			address: this.address,
-			infoType: this.infoType,
-			city: this.city,
-			state: this.state,
-			country: this.country,
-			email: this.email,
-			phone: this.phone,
-			zip: this.zip
-		});
-	};
+    };
 
-	save() {
-		var self = this;
-
-		if (this.contactInfo.id) {
-			this.httpService.http.patch(constants.DREAMFACTORY_INSTANCE_URL + '/api/v2/db/_table/contact_info', this.contactInfo.toJson(true))
-				.subscribe((data) => {
-					this.notificationService.show('success', 'Contact Info Updated!');
-				});
-		} else {
-			this.httpService.http.post(constants.DREAMFACTORY_INSTANCE_URL + '/api/v2/db/_table/contact_info/', this.contactInfo.toJson(true))
-				.subscribe((data) => {
-					this.notificationService.show('success', 'New Contact Info Created!');
-					self.back();
-				});
-		}
-
-	};
-
-	back () {
-		this.router.navigate(['/Contact', { id: this.contactInfo.contactId }]);
-	};
+    back() {
+        this.router.navigate(['/Contact', { id: this.contactInfo.contactId }]);
+    };
 }
