0a34e480d60b43db00fcff253b74432ee8e44464
Javascript Boss <javascriptboss99@gmail.com>
2016-10-08 01:14:29 +0530

    Adding session-token/content-type for API calls

--- app/components/contact-list/contact-list.ts
+++ app/components/contact-list/contact-list.ts
@@ -1,53 +1,63 @@
-import {Component, OnInit, Pipe, PipeTransform} from 'angular2/core';
-import {ROUTER_DIRECTIVES, Router} from 'angular2/router';
-
-import {URLSearchParams} from 'angular2/http';
-import {Contact} from '../../models/contact';
-import {ContactService} from '../../services/contact';
-import {BaseHttpService} from '../../services/base-http';
-
-
+import { Component } from 'angular2/core';
+import { ROUTER_DIRECTIVES, Router } from 'angular2/router';
+import { URLSearchParams } from 'angular2/http';
+import { Contact } from '../../models/contact';
+import { ContactService } from '../../services/contact';
+import { BaseHttpService } from '../../services/base-http';
+import { OrderByPipe  } from '../../models/OrderBy';
 
 @Component({
-  selector: 'contact-list',
-  templateUrl: './components/contact-list/contact-list.html',
-  styleUrls: ['./components/contact-list/contact-list.css'],
-  providers: [ContactService, BaseHttpService],
-  directives: [ROUTER_DIRECTIVES]
+    selector: 'contact-list',
+    templateUrl: './components/contact-list/contact-list.html',
+    styleUrls: ['./components/contact-list/contact-list.css'],
+    providers: [ContactService, BaseHttpService],
+    directives: [ROUTER_DIRECTIVES],
+    pipes: [OrderByPipe]
 })
 
 
 export class ContactListCmp {
-	public contacts: Contact[] = [];
-	shadowImage: string = 'https://image.freepik.com/free-icon/male-user-shadow_318-34042.png';
+    public contacts: Contact[] = [];
+    shadowImage: string = 'https://image.freepik.com/free-icon/male-user-shadow_318-34042.png';
 
-	constructor (private contactService: ContactService, private router: Router) {
-		this.getList();
-	}
+    constructor(private contactService: ContactService, private router: Router) {
+        var token = localStorage.getItem('session_token');
+        if (token === '' || token === null) {
+            this.logout();
+        } else {
+            this.getList();
+        }
+    }
+    logout() {
+        localStorage.setItem('session_token', '');
+        this.router.navigate(['Login']);
+    };
+    getList() {
+        let self = this;
+        let params: URLSearchParams = new URLSearchParams();
+        params.set('order', 'last_name+ASC');
 
-	getList () {
-		let self = this;
-		let params: URLSearchParams = new URLSearchParams();
-		params.set('order', 'last_name+ASC');
+        this.contactService.query(params)
+            .subscribe((contacts: Contact[]) => {
+                self.contacts = contacts;
+            });
+    }
 
-		this.contactService.query(params)
-			.subscribe((contacts: Contact[]) => {
-				self.contacts = contacts
-			});
-	}
+    show(contactId) {
+        this.router.navigate(['/Contact', { id: contactId }]);
+    }
 
-	show (contactId) {
-		this.router.navigate(['/Contact', { id: contactId }]);
-	}
-
-	remove (contactId) {
-		var self = this;
-		this.contactService.remove(contactId)
-			.subscribe(() => {
-				self.contacts = self.contacts.filter((item) => {
-					return item.id != contactId
-				});
-			});
-	}
+    remove(contactId) {
+    	var token = localStorage.getItem('session_token');
+        if (token ==='' || token === null) {
+            this.logout(); 
+        }
+        var self = this;
+        this.contactService.remove(contactId)
+            .subscribe(() => {
+                self.contacts = self.contacts.filter((item) => {
+                    return item.id !== contactId;
+                });
+            });
+    }
 }
-
