a37ab553a712098e0656a374c62a344aa1581f4a
Javascript Boss <javascriptboss99@gmail.com>
2016-10-08 01:16:18 +0530

    Adding session-token/content-type for API calls

--- app/components/group/group-list.ts
+++ app/components/group/group-list.ts
@@ -1,51 +1,65 @@
-import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES, Router} from 'angular2/router';
-import {URLSearchParams} from 'angular2/http';
+import { Component } from 'angular2/core';
+import { ROUTER_DIRECTIVES, Router } from 'angular2/router';
+import { URLSearchParams } from 'angular2/http';
 
-import {Group} from '../../models/group';
-import {GroupService} from '../../services/group';
-import {ContactService} from '../../services/contact';
-import {BaseHttpService} from '../../services/base-http';
+import { Group } from '../../models/group';
+import { GroupService } from '../../services/group';
+import { ContactService } from '../../services/contact';
+import { BaseHttpService } from '../../services/base-http';
+import { OrderByPipe  } from '../../models/OrderBy';
 
 
 
 @Component({
-	selector: 'group-list',
-	templateUrl: './components/group/group-list.html',
-	styleUrls: ['./components/group/group.css'],
-	providers: [GroupService, BaseHttpService, ContactService],
-	directives: [ROUTER_DIRECTIVES]
+    selector: 'group-list',
+    templateUrl: './components/group/group-list.html',
+    styleUrls: ['./components/group/group.css'],
+    providers: [GroupService, BaseHttpService, ContactService],
+    directives: [ROUTER_DIRECTIVES],
+    pipes: [OrderByPipe]
 })
 
 export class GroupListCmp {
-	public groups: Group[] = [];
-	shadowImage: string = 'https://image.freepik.com/free-icon/male-user-shadow_318-34042.png';
+    public groups: Group[] = [];
+    shadowImage: string = 'https://image.freepik.com/free-icon/male-user-shadow_318-34042.png';
 
-	constructor(private groupService: GroupService, private router: Router) {
-		this.getList();
-	}
+    constructor(private groupService: GroupService, private router: Router) {        
+        var token = localStorage.getItem('session_token');
+        if (token === '' || token === null) {
+            this.logout();
+        } else {
+            this.getList();
+        }
+    }
+    logout() {
+        localStorage.setItem('session_token', '');
+        this.router.navigate(['Login']);
+    }
+    getList() {
+        let self = this;
+        let params = new URLSearchParams();
+        params.set('order', 'name+ASC');
+        this.groupService.query(params)
+            .subscribe((groups: Group[]) => {
+                self.groups = groups;
+            });
+    }
 
-	getList() {
-		let self = this;
-		let params = new URLSearchParams();
-		params.set('order', 'name+ASC');
-		this.groupService.query(params)
-			.subscribe((groups: Group[]) => {
-				self.groups = groups
-			});
-	}
+    remove(groupId) {
+    	var token = localStorage.getItem('session_token');
+        if (token ==='' || token === null) {
+            this.logout(); 
+        }
+        var self = this;
+        this.groupService.remove(groupId)
+            .subscribe(() => {
+                self.groups = self.groups.filter((item) => {
+                    return item.id !== groupId;
+                });
+            });
+    }
 
-	remove(groupId) {
-		var self = this;
-		this.groupService.remove(groupId)
-			.subscribe(() => {
-				self.groups = self.groups.filter((item) => {
-					return item.id != groupId
-				});
-			});
-	}
-
-	show(groupId) {
-		this.router.navigate(['/Group', { id: groupId }]);
-	}
+    show(groupId) {
+        this.router.navigate(['/Group', { id: groupId }]);
+    }
 }
--- app/components/group/group.ts
+++ app/components/group/group.ts
@@ -1,122 +1,131 @@
-import {Component} from 'angular2/core';
-import {RouteParams, Router} from 'angular2/router';
-import {URLSearchParams} from 'angular2/http';
-import {FORM_DIRECTIVES, FormBuilder, Validators, Control, ControlGroup} from 'angular2/common';
+import { Component } from 'angular2/core';
+import { RouteParams, Router } from 'angular2/router';
+import { URLSearchParams } from 'angular2/http';
+import { FORM_DIRECTIVES, FormBuilder, Validators, Control, ControlGroup } from 'angular2/common';
 
-import {Group} from '../../models/group';
-import {ContactGroup} from '../../models/contact-group';
-import {Contact} from '../../models/contact';
-import {GroupService} from '../../services/group';
-import {ContactService} from '../../services/contact';
-import {ContactGroupService} from '../../services/contact-group';
+import { Group } from '../../models/group';
+import { ContactGroup } from '../../models/contact-group';
+import { Contact } from '../../models/contact';
+import { GroupService } from '../../services/group';
+import { ContactService } from '../../services/contact';
+import { ContactGroupService } from '../../services/contact-group';
 
-import {BaseHttpService} from '../../services/base-http';
-import * as constants from '../../config/constants';
+import { BaseHttpService } from '../../services/base-http';
 
 
 
 @Component({
-	selector: 'group',
-	templateUrl: './components/group/group.html',
-	styleUrls: ['./components/group/group.css'],
-	providers: [GroupService, BaseHttpService, ContactGroupService, ContactService],
-	directives: [FORM_DIRECTIVES]
+    selector: 'group',
+    templateUrl: './components/group/group.html',
+    styleUrls: ['./components/group/group.css'],
+    providers: [GroupService, BaseHttpService, ContactGroupService, ContactService],
+    directives: [FORM_DIRECTIVES]
 })
 
 export class GroupCmp {
-	form: ControlGroup;
+    form: ControlGroup;
 
-	id = new Control('');
-	name = new Control('', Validators.required);
+    id = new Control('');
+    name = new Control('', Validators.required);
 
-	selectedContactId: string = null;
+    selectedContactId: string = null;
 
-	group: Group = new Group();
-	contactGroups: Array<ContactGroup> = [];
-	remainingContacts: Array<Contact> = [];
+    group: Group = new Group();
+    contactGroups: Array < ContactGroup > = [];
+    remainingContacts: Array < Contact > = [];
 
 
-	constructor(private groupService: GroupService, private contactService: ContactService, private contactGroupService: ContactGroupService, private router: Router, private params: RouteParams, private formBuilder: FormBuilder, private httpService: BaseHttpService) {
+    constructor(private groupService: GroupService, private contactService: ContactService, private contactGroupService: ContactGroupService,
+        private router: Router, private params: RouteParams, private formBuilder: FormBuilder, private httpService: BaseHttpService) {
 
-		var groupId: string = params.get('id');
+        var token = localStorage.getItem('session_token');
+        if (token === '' || token === null) {
+            this.logout();
+        }
+        var groupId: string = params.get('id');
 
-		if (groupId) {
-			let self = this;
-			var contactGroupParams = new URLSearchParams();
-			contactGroupParams.set('filter', 'contact_group_id=' + groupId);
+        if (groupId) {
+            let self = this;
+            var contactGroupParams = new URLSearchParams();
+            contactGroupParams.set('filter', 'contact_group_id=' + groupId);
 
-			groupService
-				.get(groupId)
-				.subscribe((group) => self.group = group);
+            groupService
+                .get(groupId)
+                .subscribe((group) => self.group = group);
 
-			contactGroupService
-				.query(contactGroupParams, true)
-				.subscribe((contactGroups) => {
-					self.contactGroups = contactGroups;
-					self.getRemainingContacts();
-				});
-		}
+            contactGroupService
+                .query(contactGroupParams, true)
+                .subscribe((contactGroups) => {
+                    self.contactGroups = contactGroups;
+                    self.getRemainingContacts();
+                });
+        }
 
-		this.form = this.formBuilder.group({
-			name: this.name
-		});
-	}
+        this.form = this.formBuilder.group({
+            name: this.name
+        });
+    };
+    
+    logout() {
+        localStorage.setItem('session_token', '');
+        this.router.navigate(['Login']);
+    };
 
-	back() {
-		this.router.navigate(['/GroupList']);
-	}
+    back() {
+        this.router.navigate(['/GroupList']);
+    }
 
-	getRemainingContacts() {
-		var self = this;
-		this.contactService
-			.query()
-			.subscribe((contacts) => {
-				self.remainingContacts = contacts.filter((item) => {
-					return !self.contactGroups.some((a) => {
-						return a.group.id == item.id;
-					});
-				});
-			})
-	}
+    getRemainingContacts() {
+        var self = this;
+        this.contactService
+            .query()
+            .subscribe((contacts) => {
+                self.remainingContacts = contacts.filter((item) => {
+                    return !self.contactGroups.some((a) => {
+                        return a.group.id === item.id;
+                    });
+                });
+            });
+    };
 
-	addSelectedContact() {
-		if (!this.selectedContactId) return;
+    addSelectedContact() {
+        if (!this.selectedContactId) return;
 
-		var self = this;
-		var contact = this.remainingContacts.filter((item) => {
-			return item.id == self.selectedContactId;
-		})[0];
+        var self = this;
+        var contact = this.remainingContacts.filter((item) => {
+            return item.id === self.selectedContactId;
+        })[0];
 
-		this.contactGroupService.addGroup(this.group.id, contact.id)
-			.subscribe((response) => {
-				var newContactGroup = new ContactGroup(response.id);
-				newContactGroup.contact = contact;
+        this.contactGroupService.addGroup(this.group.id, contact.id)
+            .subscribe((response) => {
+                var newContactGroup = new ContactGroup(response.id);
+                newContactGroup.contact = contact;
 
-				self.remainingContacts = self.remainingContacts.filter((item) => {
-					return item.id !== contact.id;
-				});
+                self.remainingContacts = self.remainingContacts.filter((item) => {
+                    return item.id !== contact.id;
+                });
 
-				self.contactGroups.unshift(newContactGroup);
-				self.selectedContactId = null;
-			});
-	}
+                self.contactGroups.unshift(newContactGroup);
+                self.selectedContactId = null;
+            });
+    };
 
-	removeContact(id) {
-		var self = this;
-		this.contactGroupService
-			.remove(id)
-			.subscribe(() => {
-				self.contactGroups = self.contactGroups.filter((item) => {
-					return item.id !== id;
-				});
-			});
-	};
+    removeContact(id) {
+        var self = this;
+        this.contactGroupService
+            .remove(id)
+            .subscribe(() => {
+                self.contactGroups = self.contactGroups.filter((item) => {
+                    return item.id !== id;
+                });
+            });
+    };
 
-	save() {
-		var self = this;
-		this.groupService.save(this.group)
-			.subscribe((response) => {
-				self.back();
-			});
-	}
+    save() {
+        var self = this;
+        this.groupService.save(this.group)
+            .subscribe((response) => {
+                self.back();
+            });
+    };
 }
