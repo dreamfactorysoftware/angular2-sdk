8141d3693f48600a826cb85c8bea1e609a94c24d
Javascript Boss <javascriptboss99@gmail.com>
2016-10-08 01:17:32 +0530

    Adding session-token/content-type for API calls

--- app/components/register/register.ts
+++ app/components/register/register.ts
@@ -1,44 +1,49 @@
-import {Component} from 'angular2/core';
-import {FORM_DIRECTIVES, FormBuilder, Validators, Control, ControlGroup} from 'angular2/common';
-import {Router, ROUTER_DIRECTIVES} from 'angular2/router';
-import {BaseHttpService} from '../../services/base-http';
+import { Component } from 'angular2/core';
+import { FormBuilder, Validators, Control, ControlGroup } from 'angular2/common';
+import { Router, ROUTER_DIRECTIVES } from 'angular2/router';
+import { Headers, RequestOptions } from 'angular2/http';
+import { BaseHttpService } from '../../services/base-http';
 import * as constants from '../../config/constants';
-import {NotificationService} from '../../services/notification';
-import {BrowserDomAdapter} from 'angular2/platform/browser';
+import { NotificationService } from '../../services/notification';
+import { ValidationService } from '../../services/validation';
+import { BrowserDomAdapter } from 'angular2/platform/browser';
 
 @Component({
-  selector: 'df-register',
-  templateUrl: './components/register/register.html',
-  styleUrls: ['./components/register/register.css'],
-  providers: [BaseHttpService, NotificationService, BrowserDomAdapter],
-  directives: [ROUTER_DIRECTIVES]
+    selector: 'df-register',
+    templateUrl: './components/register/register.html',
+    styleUrls: ['./components/register/register.css'],
+    providers: [BaseHttpService, NotificationService, BrowserDomAdapter],
+    directives: [ROUTER_DIRECTIVES]
 })
 
 export class RegisterCmp {
-	form: ControlGroup;
+    form: ControlGroup;
 
-	constructor(private httpService: BaseHttpService, private formBuilder: FormBuilder, private router: Router, private notificationService: NotificationService) {
-		this.form = formBuilder.group({
-			first_name: new Control('', Validators.required),
-			last_name: new Control(''),
-			email: new Control('', Validators.required),
-			password: new Control('', Validators.required)
-		});
-	}
+    constructor(private httpService: BaseHttpService, private formBuilder: FormBuilder, private router: Router,
+        private notificationService: NotificationService) {
+        this.form = formBuilder.group({
+            first_name: new Control('', Validators.compose([Validators.minLength(3), Validators.maxLength(50), Validators.required])),
+            last_name: new Control(''),
+            email: new Control('', Validators.compose([Validators.maxLength(50), ValidationService.emailValidator, Validators.required])),
+            password: new Control('', Validators.compose([Validators.minLength(6), Validators.maxLength(50), Validators.required]))
+        });
+    }
 
-	private storeToken(data) {
-		this.httpService.http._defaultOptions.headers.set('X-Dreamfactory-Session-Token', data && data.session_token);
-		localStorage.setItem('session_token', data.session_token);
-		this.router.navigate(['ContactList']);
-	}
+    private storeToken(data) {
+        localStorage.setItem('session_token', data.session_token);
+        this.router.navigate(['GroupList']);
+    }
 
-	register () {
-		this.httpService.http
-			.post(constants.DREAMFACTORY_INSTANCE_URL + '/api/v2/user/register?login=true', JSON.stringify(this.form.value))
-			.subscribe((response) => {
-				this.storeToken(response.json());
-			}, (error) => {
-				this.notificationService.show('error', 'Cannot register new user, try again!');
-			});
-	}
+    register() {
+        var queryHeaders = new Headers();
+        queryHeaders.append('Content-Type', 'application/json');
+        let options = new RequestOptions({ headers: queryHeaders });
+        this.httpService.http
+            .post(constants.DREAMFACTORY_INSTANCE_URL + '/api/v2/user/register?login=true', JSON.stringify(this.form.value), options)
+            .subscribe((response) => {
+                this.storeToken(response.json());
+            }, (error) => {
+                this.notificationService.show('error', JSON.parse(error._body).error.message);
+            });
+    }
 }
